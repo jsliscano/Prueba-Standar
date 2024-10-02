9be4d530b5ff5806031483d5c6fb911012b407b6
jsliscano <estivenliscano2017@gmail.com>
2024-10-02 11:26:22 -0500

    generacion del proyecto

---
+++ src/main/java/com/example/PruebaStandar/PruebaStandarApplication.java
@@ -0,0 +1,13 @@
+package com.example.PruebaStandar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaStandarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaStandarApplication.class, args);
+	}
+
+}
